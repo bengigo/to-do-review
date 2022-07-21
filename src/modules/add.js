1b6f0e672011f7fd910ef4e298b715f32249c212
bengigenc <genc.bengi@gmail.com>
2022-07-21 16:18:33 +0300

    Fix linter issues

--- src/modules/add.js
+++ src/modules/add.js
@@ -5,28 +5,27 @@ export default function addToList() {
   const taskInput = document.querySelector('#task-input');
   const addIcon = document.querySelector('#add');
   const messageBox = document.querySelector('#message-display');
-  if(messageBox) {
+  if (messageBox) {
     messageBox.style.display = 'none';
   }
 
- if (addIcon) {
-  addIcon.addEventListener('click', () => {
-    const toDos = getStorage();
-    if (taskInput.value !== '') {
-      const newTask = new Task(taskInput.value);
-      toDos.push(newTask);
-      toDos.forEach((obj, i) => {
-        obj.index = i + 1;
-      });
-      localStorage.setItem('toDos', JSON.stringify(toDos));
-      taskInput.value = '';
-      window.location.reload();
-    } else {
-      messageBox.style.display = 'flex';
-      messageBox.innerText = "You didn't write anything!";
-      messageBox.style.color = 'tomato';
-    }
-  });
-
- }
+  if (addIcon) {
+    addIcon.addEventListener('click', () => {
+      const toDos = getStorage();
+      if (taskInput.value !== '') {
+        const newTask = new Task(taskInput.value);
+        toDos.push(newTask);
+        toDos.forEach((obj, i) => {
+          obj.index = i + 1;
+        });
+        localStorage.setItem('toDos', JSON.stringify(toDos));
+        taskInput.value = '';
+        window.location.reload();
+      } else {
+        messageBox.style.display = 'flex';
+        messageBox.innerText = "You didn't write anything!";
+        messageBox.style.color = 'tomato';
+      }
+    });
+  }
 }
