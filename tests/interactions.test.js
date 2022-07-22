a9e02d3fae32f2655db54a3b461c242622aa6d62
bengigenc <genc.bengi@gmail.com>
2022-07-22 11:27:11 +0300

    Solo practice

--- tests/interactions.test.js
+++ tests/interactions.test.js
@@ -2,4 +2,14 @@
  * @jest-environment jsdom
  */
 
-import { check, uncheck } from "../src/modules/interactions";
+import addToList from "../src/modules/__mocks__/add-remove";
+import interactions from "../src/modules/interactions";
+
+test('should change completed value', () => {
+    document.body.innerHTML = `
+    <div class="task-info">
+      <input class="checkbox" type="checkbox">
+      <input class="editable" value="finish testing"></input>
+    </div>
+    `
+})
